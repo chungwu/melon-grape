0453541cca82c0b0d1173e2164c7a607470c3f41
Chung Wu <chungwu@gmail.com>
2011-12-27 10:13:38 -0800

    Paper stacks

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -42,5 +42,32 @@ function animateClasses(selector, duration, startClass, endClass) {
 function openPage($page) {
   $(".page-opened").removeClass("page-opened").addClass("page-closed");
   $page.addClass("page-opened").removeClass("page-closed");
-  $(".pages-container").css("height", $page.outerHeight() + "px");  
+  $(".sand-background").css("height", $page.outerHeight() + "px");
+}
+
+function fillStack() {
+  var $stack = $(".stack");
+  $stack.css("top", $(".page").last().position().top + 50 + "px");
+  var images = [
+    "images/paper1b.jpg",
+    "images/paper2.jpg",
+    "images/paper3.jpg",
+    "images/paper4.jpg",
+    "images/paper5.jpg",
+    "images/paper6.jpg",
+    "images/paper7b.jpg"
+  ];
+  var lastTop = 0;
+  for (var i = 0; i < 50; i++) {
+    var $page = $("<div/>").addClass("page page-closed").css(
+      {
+	top: lastTop - 5 + Math.random() * 10 + "px",
+	left: -780 + Math.random()*20 + "px"
+      }).appendTo($stack);
+    var $header = $("<div class='page-header'><div class='page-title'>&nbsp;</div></div>").appendTo($page);
+    $header.css("backgroundImage", "url('" +  images[Math.floor(Math.random() * images.length)] + "')");
+    $header.css("backgroundPosition", "0px " + Math.random() * -600 + "px");
+    $("<div class='page-content'/>").appendTo($page);
+    lastTop += 15;
+  }
 }
